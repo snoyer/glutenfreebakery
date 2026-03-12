c20642ce5735d1b40e6a39a2caae9604b15e900f
snoyer <noyer.stephane@gmail.com>
2026-03-12 11:41:31 +0400

    simplify URI reading and improve testing

--- glutenfreebakery/util.py
+++ glutenfreebakery/util.py
@@ -1,10 +1,9 @@
 import base64
 import logging
 import mimetypes
-from urllib.error import URLError
-import urllib.parse
 import urllib.request
 from pathlib import Path
+from urllib.error import URLError
 
 logger = logging.getLogger(__name__)
 
@@ -13,23 +12,17 @@ def read_uri_data(uri: str, relative_to: Path | None = None):
     mimetype, _encoding = mimetypes.guess_type(uri)
     mimetype = mimetype or "application/octet-stream"
 
-    def uri_is_file():
-        try:
-            url = urllib.parse.urlparse(uri, "file")
-            if url.scheme == "file":
-                return url.path
-        except URLError:  # can be caused by Windows drive letter
-            return uri
+    if not uri.startswith("data:"):
+        logger.info("reading %r", uri)
 
-    if path := uri_is_file():
-        fixed_path = Path(relative_to or ".") / path
+    try:
+        return urllib.request.urlopen(uri).read(), mimetype
+    except ValueError:
+        fixed_path = Path(relative_to or ".") / uri
         try:
             return fixed_path.read_bytes(), mimetype
         except IOError as e:
             raise URLError(e.strerror or "unknown IO error", uri)
-    else:
-        logger.info("reading %s", uri)
-        return urllib.request.urlopen(uri).read(), mimetype
 
 
 def guess_extension(mime_type: str, default: str = ".bin"):
